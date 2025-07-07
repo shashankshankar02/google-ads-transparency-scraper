97524608b2e44ba271c360f18bd7984c4f654cb6
CUBE <cube@CUBEs-MacBook-Pro.local>
2025-07-07 08:36:31 +0530

    Update WebDriver configuration to use Chrome Options for capabilities

--- api.py
+++ api.py
@@ -71,6 +71,11 @@ async def setup_browser() -> webdriver.Remote:
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    
+    # Set capabilities through options
+    chrome_options.set_capability('browserName', 'chrome')
+    chrome_options.set_capability('browserVersion', 'latest')
     
     # Get browserless token from environment variable
     browserless_token = os.getenv('BROWSERLESS_TOKEN')
